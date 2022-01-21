1a9e5953a2080eb9f2fcb5c7963604e4c6eb33ae
alex smith <alex.smith4640@yahoo.com>
2022-01-20 21:17:30 -0600

    built skeleton for content wrapper

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 
 import Header from './header';
 import Footer from './footer';
+import ContentWrapper from './content/contentWrapper';
 
 
 import dummyData from "../../static/assets/dummyData";
@@ -20,7 +21,15 @@ export default class App extends Component {
     return (
       <div className='page-wrapper'>
         <Header month={this.state.monthData.name} />
+        <ContentWrapper
+    
+        daysInMonth={this.state.monthData.days_in_month}
+        daysInPreviousMonth={this.state.monthData.days_in_previous_month}
+        startDay={this.state.monthData.start_day}
+    
+        />
         <Footer year={this.state.monthData.year} />
+
       </div>
     );
   }
---
+++ src/components/content/contentWrapper.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import dummyData from "../../../static/assets/dummyData";
+
+export default function contentWrapper(props) {
+    return (
+        <div className="content-wrapper">
+            
+        </div>
+    )
+}
