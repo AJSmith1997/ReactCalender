9a490786f45e2cbd9c449974c4a4902eebe7e770
alex smith <alex.smith4640@yahoo.com>
2022-01-20 21:30:52 -0600

    added day of week component

--- src/components/content/contentWrapper.js
+++ src/components/content/contentWrapper.js
@@ -1,11 +1,21 @@
 import React from "react";
 
+import DayOfWeek from "./dayOfWeek";
+
+
 import dummyData from "../../../static/assets/dummyData";
 
 export default function contentWrapper(props) {
     return (
         <div className="content-wrapper">
-            
+            <DayOfWeek day="Sunday" />
+            <DayOfWeek day="Monday" />       
+            <DayOfWeek day="Tuesday" />       
+            <DayOfWeek day="Wednesday" />       
+            <DayOfWeek day="Thursday" />       
+            <DayOfWeek day="Friday" />       
+            <DayOfWeek day="Saturday" />       
+
         </div>
     )
 }
