7a77453014e44a0f8ceefcc785d3e4afbc931b79
alex smith <alex.smith4640@yahoo.com>
2022-01-21 20:17:18 -0600

    added styles

--- src/components/content/contentWrapper.js
+++ src/components/content/contentWrapper.js
@@ -6,6 +6,52 @@ import CalenderBox from "./calenderBox";
 import dummyData from "../../../static/assets/dummyData";
 
 export default function contentWrapper(props) {
+    
+    const renderPreviousCalenderBoxes = () => {
+        const PreviousCalenderBoxes = []
+        
+        for (let i=1; i<=props.startDay; i++) {
+            PreviousCalenderBoxes.push(
+                <CalenderBox date={props.daysInPreviousMonth - (props.startDay - i)} />
+            )
+        }
+
+
+        return PreviousCalenderBoxes
+    }
+    
+    
+    
+    const renderCalenderBoxes = () => {
+        const calenderBoxes = []
+
+        for (let i=1; i<=props.daysInMonth; i++) {
+            calenderBoxes.push(
+                <CalenderBox date={i}/>
+            )
+        }
+
+        return calenderBoxes
+    }
+
+    const renderNextCalenderBoxes = () => {
+        const nextCalenderBoxes = []
+
+        for (let i=1; i<=(42 - props.daysInMonth - props.startDay); i++) {
+            nextCalenderBoxes.push(
+                <CalenderBox date={i} />
+            )
+        }
+
+        return nextCalenderBoxes
+
+
+
+    }
+
+
+
+
     return (
         <div className="content-wrapper">
             <DayOfWeek day="Sunday" />
@@ -15,7 +61,9 @@ export default function contentWrapper(props) {
             <DayOfWeek day="Thursday" />       
             <DayOfWeek day="Friday" />       
             <DayOfWeek day="Saturday" />
-            <CalenderBox date="1" />
+            {renderPreviousCalenderBoxes()}
+            {renderCalenderBoxes()}
+            {renderNextCalenderBoxes()}
 
         </div>
     )
